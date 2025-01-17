d2505d58aa2e209f603a0ee5962690aebbb8ab2a
AnemonOFF <anemonoff@users.noreply.github.com>
2025-01-17 18:46:49 +0500

    Fix NextJS 15 async params

--- src/app/(authenticated)/gamepacks/[id]/page.tsx
+++ src/app/(authenticated)/gamepacks/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { GamePackPage } from "@/view/gamePacks";
 import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const idInt = parseInt(params.id);
   if (isNaN(idInt)) notFound();
 
--- src/app/(authenticated)/games/[id]/page.tsx
+++ src/app/(authenticated)/games/[id]/page.tsx
@@ -1,7 +1,8 @@
 import { GamePage } from "@/view/games";
 import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const idInt = parseInt(params.id);
   if (isNaN(idInt)) notFound();
 
