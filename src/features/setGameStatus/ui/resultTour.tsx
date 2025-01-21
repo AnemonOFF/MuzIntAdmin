cd25c82a48f590d5ecbbf83b7f8914137d22d8dc
AnemonOFF <anemonoff@users.noreply.github.com>
2025-01-22 01:51:34 +0500

    end tour warning player not ready

--- src/features/setGameStatus/ui/resultTour.tsx
+++ src/features/setGameStatus/ui/resultTour.tsx
@@ -3,11 +3,14 @@
 import { useGameStatusMutation, useGameStore } from "@/entities/game";
 import { GameStatus } from "@/shared/types/game";
 import { Button } from "@/shared/ui/button";
-import React from "react";
+import Modal from "@/shared/ui/modal";
+import React, { useState } from "react";
 
 export interface TourResultProps {}
 
 const TourResult: React.FC<TourResultProps> = ({}) => {
+  const currentTourId = useGameStore((state) => state.currentTourId);
+  const [open, setOpen] = useState(false);
   const { mutate, isPending } = useGameStatusMutation();
   const gameId = useGameStore((state) => state.id);
 
@@ -18,10 +21,48 @@ const TourResult: React.FC<TourResultProps> = ({}) => {
     });
   };
 
+  const verifyPlayers = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    const players = useGameStore.getState().players;
+    if (
+      players.some(
+        (p) =>
+          !p.playerTours.find((t) => t.tourId === currentTourId)?.isAnswered
+      )
+    ) {
+      setOpen(true);
+      return;
+    }
+    endTour();
+  };
+
   return (
-    <Button onClick={endTour} disabled={isPending}>
-      Закончить тур
-    </Button>
+    <Modal
+      open={open}
+      onOpenChange={setOpen}
+      trigger={
+        <Button onClick={verifyPlayers} disabled={isPending}>
+          Закончить тур
+        </Button>
+      }
+      title="Закончить тур"
+      content={
+        <div className="space-y-5">
+          <span>
+            Не все игроки сохранили ответы, им будем засчитано 0 баллов, вы
+            уверены?
+          </span>
+          <div className="flex items-center justify-between gap-2">
+            <Button variant="outline" onClick={endTour}>
+              Закончить
+            </Button>
+            <Button onClick={() => setOpen(false)}>Отмена</Button>
+          </div>
+        </div>
+      }
+    />
   );
 };
 
