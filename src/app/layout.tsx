83dd973dbbca211bb09ccff9a8ba6d200d09521c
AnemonOFF <anemonoff@users.noreply.github.com>
2025-01-22 20:21:10 +0500

    noindex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ export const metadata: Metadata = {
     default: "МузИнт - музыкальная интуиция",
   },
   description: "Игра - музыкальная интуиция",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
