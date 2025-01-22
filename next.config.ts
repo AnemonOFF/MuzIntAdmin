2259eae96309ea9a4ed79cd3d2b0975292fb43c4
AnemonOFF <anemonoff@users.noreply.github.com>
2025-01-22 14:03:24 +0500

    dockerize

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  basePath: "/admin",
   async redirects() {
     return [
       {
